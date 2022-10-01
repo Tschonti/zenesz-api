164c744ecbc2acb8f7e54e56ca60cab8b5fa28ad
Sámuel Fekete <feketesamu@gmail.com>
2022-10-01 16:07:33 +0200

    stuff

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -139,6 +139,8 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = "/static/"
 
+
+
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
